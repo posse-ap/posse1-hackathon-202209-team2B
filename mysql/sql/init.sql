cb6c03d727b2371b7d334cdc24541282abf42ab2
manaki-e <48690114s@gmail.com>
2022-09-07 07:03:45 +0900

    usersテーブル設計

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -2,6 +2,17 @@ DROP SCHEMA IF EXISTS posse;
 CREATE SCHEMA posse;
 USE posse;
 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(10) NOT NULL,
+  email  VARCHAR(16) COLLATE utf8_bin,
+  password VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at DATETIME
+);
+
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -23,6 +34,9 @@ CREATE TABLE event_attendance (
   deleted_at DATETIME
 );
 
+
+INSERT INTO users SET name='石川朝香', email='asaka@posse.com', password='eddy';
+INSERT INTO users SET name='尾関なな海', email='nanami@posse.com', password='minn';
 
 INSERT INTO events SET name='縦モク', start_at='2021/08/01 21:00', end_at='2021/08/01 23:00';
 INSERT INTO events SET name='横モク', start_at='2021/08/02 21:00', end_at='2021/08/02 23:00';
