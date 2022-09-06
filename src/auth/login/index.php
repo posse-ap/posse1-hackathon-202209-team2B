7cadd4ea290b3693299f4e897b51661af65688d8
manaki-e <48690114s@gmail.com>
2022-09-07 07:32:49 +0900

    #7 基本的なログイン機能作成

--- src/auth/login/index.php
+++ src/auth/login/index.php
@@ -1,3 +1,59 @@
+<?php
+require "../../dbconnect.php";
+
+session_start();
+
+// セッション変数 $_SESSION["loggedin"]を確認。ログイン済だったらウェルカムページへリダイレクト
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+  header("location: index.php");
+  exit;
+}
+
+//POSTされてきたデータを格納する変数の定義と初期化
+$datas = [
+  'email'  => '',
+  'password'  => ''
+];
+$login_err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  // POSTされてきたデータを変数に格納
+  foreach ($datas as $key => $value) {
+    if ($value = filter_input(INPUT_POST, $key, FILTER_DEFAULT)) {
+      $datas[$key] = $value;
+    }
+  }
+
+  //ユーザーネームから該当するユーザー情報を取得
+  $sql = "SELECT id,name,password FROM users WHERE email = :email";
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue('email', $datas['email']);
+  $stmt->execute();
+
+  //ユーザー情報があれば変数に格納
+  if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    //パスワードがあっているか確認
+    if ($datas['password'] === $row['password']) {
+
+      //セッション変数にログイン情報を格納
+      $_SESSION["loggedin"] = true;
+      $_SESSION["id"] = $row['id'];
+      $_SESSION["name"] =  $row['name'];
+      header("location: ../../index.php");
+      exit();
+    } else {
+      $login_err = 'Invalid username or password.';
+    }
+  } else {
+    $login_err = 'Invalid username or password.';
+  }
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -21,12 +77,19 @@
   <main class="bg-gray-100 h-screen">
     <div class="w-full mx-auto py-10 px-5">
       <h2 class="text-md font-bold mb-5">ログイン</h2>
-      <form action="/" method="POST">
-        <input type="email" placeholder="メールアドレス" class="w-full p-4 text-sm mb-3">
-        <input type="password" placeholder="パスワード" class="w-full p-4 text-sm mb-3">
+
+      <?php
+      if (!empty($login_err)) {
+        echo '<div class="alert alert-danger">' . $login_err . '</div>';
+      }
+      ?>
+
+      <form action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="POST">
+        <input type="email" name="email" placeholder="メールアドレス" class="w-full p-4 text-sm mb-3">
+        <input type="password" name="password" placeholder="パスワード" class="w-full p-4 text-sm mb-3">
         <label class="inline-block mb-6">
-          <input type="checkbox" checked>
-          <span class="text-sm">ログイン状態を保持する</span>
+          <!-- <input type="checkbox" checked> -->
+          <!-- <span class="text-sm">ログイン状態を保持する</span> -->
         </label>
         <input type="submit" value="ログイン" class="cursor-pointer w-full p-3 text-md text-white bg-blue-400 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-300">
       </form>
