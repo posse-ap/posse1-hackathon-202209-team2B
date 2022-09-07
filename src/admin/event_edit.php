69d4e257a0b62729b5bb6a0c294aebd64bed3340
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-09-07 19:39:01 +0900

    編集機能SQL書いた

--- src/admin/event_edit.php
+++ src/admin/event_edit.php
@@ -3,11 +3,68 @@ require('../dbconnect.php');
 
 if (isset($_GET['id'])) {
   $id = htmlspecialchars($_GET['id']);
-
   $stmt = $db->prepare('SELECT * FROM events WHERE id = ?');
   $stmt->execute(array($id));
   $event = $stmt->fetchAll(pdo::FETCH_ASSOC);
 }
+
+if (isset($_POST['name'])) {
+  $id = $event[0]['id'];
+
+  try {
+      // 送信された値を取得
+      $name = $_POST['name'];
+      $start_at = $_POST['start_at'];
+      $end_at = $_POST['end_at'];
+      $event_detail = $_POST['event_detail'];
+
+      echo $event_detail;
+
+      $sql = 'UPDATE events SET
+      name = :name, start_at = :start_at, end_at = :end_at, event_detail = :event_detail WHERE id = :id';
+      $stmt = $db->prepare($sql);
+      $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+      $stmt->bindValue(":name",  $name, PDO::PARAM_STR);
+      // 
+      $stmt->bindValue(":start_at",  $start_at);
+      // 
+      $stmt->bindValue(":end_at",  $end_at);
+      // 
+      $stmt->bindValue(":event_detail",  $event_detail);
+      $stmt->execute();
+
+      echo '更新しました';
+      exit();
+  } catch (PDOException $e) {
+      exit('データベースに接続できませんでした。' . $e->getMessage());
+  }
+}
+
+// if (isset($_POST['name'])) {
+//   $db->beginTransaction();
+//   $name = $_POST['name'];
+//     try {
+//       $stmt = $db->prepare('UPDATE events SET name = :name, start_at = :start_at, end_at = :end_at, event_detail = :event_detail WHERE id = :id');
+      
+//       $id = $event[0]['id'];
+//       $name = $_POST['name'];
+//       $start_at = $_POST['start_at'];
+//       $end_at = $_POST['end_at'];
+//       $event_detail = $_POST['event_detail'];
+
+//       $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+//       $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+//       $stmt->bindParam(':start_at', $start_at);
+//       $stmt->bindParam(':end_at', $end_at);
+//       $stmt->bindParam(':event_detail', $event_detail, PDO::PARAM_STR);
+
+//       $db->commit();
+//       echo '更新しました';
+//     } catch (PDOException $e) {
+//       echo 'データベースにアクセスできません！'.$e->getMessage();
+//     }
+//   }
 
 ?>
 
@@ -38,26 +95,23 @@ if (isset($_GET['id'])) {
     <div>
       <h1>イベント変更</h1>
     </div>
-    <form action="./event_edit.php" method="post" class="registration_form">
+    <form action="./event_edit.php" method="POST" class="registration_form">
       <div>
-        <p>イベント名<br><input type="text" value="<?php echo $event[0]['name'] ?>"></p>
+        <p>イベント名<br><input name="name" type="text" value="<?php echo $event[0]['name'] ?>"></p>
       </div>
       <div>
-        <p>開始時間<br><input type="datetime-local" value="<?php echo $event[0]['start_at'] ?>"></p>
+        <p>開始時間<br><input name="start_at" type="datetime-local" value="<?php echo $event[0]['start_at'] ?>"></p>
       </div>
       <div>
-        <p>終了時間<br><input type="datetime-local" value="<?php echo $event[0]['end_at'] ?>"></p>
+        <p>終了時間<br><input name="end_at" type="datetime-local" value="<?php echo $event[0]['end_at'] ?>"></p>
       </div>
       <div>
-        <p>イベント内容<br><input type="text" value="<?php echo $event[0]['event_detail'] ?>"></p>
+        <p>イベント内容<br><input name="event_detail" type="text" value="<?php echo $event[0]['event_detail'] ?>"></p>
       </div>
       <div>
-        <input type="submit" value="変更">
+        <button name="update" type="submit">変更</button>
       </div>
       </form>
-
-
 </main>
 </body>
-
 </html>
