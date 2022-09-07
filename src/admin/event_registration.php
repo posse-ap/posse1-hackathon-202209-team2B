469ad4d38172a45c46e9c6029fb20431c7286e18
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-09-07 13:30:58 +0900

    フロントの実装

---
+++ src/admin/event_registration.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../admin/css/admin.css">
+  <title>イベント登録</title>
+</head>
+
+<body>
+  <header>
+    <div class="header_logo">
+      <img src="../img/header-logo.png" alt="posseロゴ">
+    </div>
+    <div class="header_title">
+      <!-- 後に変更！！！！！！！！！！！！！！！！！！ -->
+      <p><a href="event_registration.html">イベント登録</a></p>
+    </div>
+    <!-- <div class="header_title"> -->
+    <!-- 後に変更！！！！！！！！！！！！！！！！！！ -->
+    <!-- <p><a href="event_registration.html">イベント一覧</a></p>
+    </div> -->
+  </header>
+  <main>
+  <div>
+    <h1>イベント登録</h1>
+  </div>
+  <!-- get, post???????? -->
+  <form action="" method="get" class="registration_form">
+    <div>
+      <p>イベント名<br><input type="text"></p>
+    </div>
+    <div>
+      <p>開始時間<br><input type="datetime-local"></p>
+    </div>
+    <div>
+      <p>終了時間<br><input type="datetime-local"></p>
+    </div>
+    <div>
+      <input type="submit" value="送信">
+    </div>
+  </form>
+
+
+</main>
+</body>
+
+</html>
