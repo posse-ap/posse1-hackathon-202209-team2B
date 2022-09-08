c4b291317ebf73d3cc4a33fda6e5c8553d7e914b
manaki-e <48690114s@gmail.com>
2022-09-08 23:38:47 +0900

    githubログイン

--- src/auth/login/github.php
+++ src/auth/login/github.php
@@ -8,7 +8,7 @@ if (empty($_GET['code'])) {
     'client_id' => CLIENT_ID,
   );
   $authorizeUrl = 'https://github.com/login/oauth/authorize?' . http_build_query($params);
-  header('Location: ' . $authorizeUrl);
+  // header('Location: ' . $authorizeUrl);
 } else {
   // アクセストークン取得
   $accessTokenUrl = 'https://github.com/login/oauth/access_token';
@@ -35,17 +35,4 @@ if (empty($_GET['code'])) {
 
 
   $url = 'https://api.github.com/user?access_token=' . $accessToken;
-
-  $ch = curl_init(); //開始
-
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 証明書の検証を行わない
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // curl_execの結果を文字列で返す
-
-  $response =  curl_exec($ch);
-  $result = json_decode($response, true);
-
-  curl_close($ch); //終了
-  echo $ch;
 }
